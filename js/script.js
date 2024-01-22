952c42f5ff0ae1c7c533a3ce12144d70e00c53ef
DeffreusTheda <d3ffr3u5@proton.me>
2024-01-22 07:44:30 +0700

    rebasinging
    Signed-off-by: DeffreusTheda <d3ffr3u5@proton.me>

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let sections = document.querySelectorAll('section');
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+
+    if(top >= offset && top < offset + height){
+      let animateElements = document.querySelectorAll('.animate');
+      animateElements.forEach(element => {
+        element.style.transform = 'translateX(0)';
+        element.style.opacity = '1';
+      });
+    }
+    if(top >= offset && top < offset + height){
+      let animateElements = document.querySelectorAll('.animate_desc');
+      animateElements.forEach(element => {
+        element.style.transform = 'translateX(0)';
+        element.style.opacity = '1';
+        element.style.backgroundColor = '#FF88BB';
+      });
+    }
+  });
+}
